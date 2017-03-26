e53d83d1c1df88d79c17c4361f59470e4f247b47
Mark McDermott <markmcdermott@Marks-MacBook-Pro.local>
2017-03-25 22:31:22 -0500

    first half of will's suggestions

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
 (function(){
 
   // init vars
-  var canvas = document.getElementById("canvas"),
+  const canvas = document.getElementById("canvas"),
       ctx = canvas.getContext("2d"),
       canWidth = canvas.width,
-      pixel = canWidth / 10.0, // unit for width, 1/10 width of board
-      frame = 0,
+
+      // "Pixel" is unit of height/width, 1/10 width of board.
+      // Each block is made of 4 pixels.
+      pixel = canWidth / 10.0;
+
+      // frame counter (needed for block entrance timing)
+  let frame = 0,
+
+      // 2d array of board layout for keeping track
+      // of all "landed" blocks.
+      // Landed blocks are blocks that have hit
+      // the floor or hit other blocks collected at bottom.
+      //
+      // landed array is all 0's to start, since no
+      // blocks have hit the floor.  Every coordinate
+      // with a landed block will gets a 1.
       landed = [
         [0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0],
@@ -30,58 +44,68 @@
       ],
 
       // init blocks
-      // using letter names for blocks, see http://i.imgur.com/9Z0oJXe.png
+      // Using letter names for blocks, see http://i.imgur.com/9Z0oJXe.png
+      // A lot of stuff in Block class, but the big one is coordinate property
+      // All block movement/collision calculated from block coordiates.
+      // Blocks are made of 4 "pixels"
+      // First block "pixel" is bottom left pixel.
+      // Subsequent ones are calculated from first pixel
       Block = {
-        i: function(x,y){
-          this.form = [
-            [1,1,1,1]
+        i: function(x, y) {
+          this.form = [   // this is the visual layout of this block
+            [ 1, 1, 1, 1 ]
           ];
-          this.height = 1;
-          this.width = 3;
-          this.num = 4;
-          this.initPos = [x,y];
-          this.coords = [
-            [x,y],[x+1,y],[x+2,y],[x+3,y]
+          this.height = 1;      // block height (used for floor collision math)
+          this.width = 3;       // block width (used for side wall collision math)
+          this.num = 4;         // this is num pixels in this block, change to numPix i think
+          this.initPos = [ x, y ]; // init block position (eventually may not be necessary)
+          this.coords = [       // block's coordiates.  used for most block movement calculations
+            [ x, y ], [ x + 1, y ], [ x + 2, y ], [ x + 3, y ]
           ];
+
+          // rotation positions
+          // todo: too many "magic numbers" here, use array forms
+          // to make this visual and so quickly understandable
           this.positions = [
-            [ [x,y],[x+1,y],[x+2,y],[x+3,y] ],
-            [ [x+1,y-1],[x+1,y],[x+1,y+1],[x+1,y+2] ],
-            [ [x,y],[x,y],[x,y],[x,y] ]
+            [ [ x , y ], [ x + 1, y ], [ x + 2, y ], [ x + 3, y ] ],
+            [ [ x + 1, y - 1 ], [ x + 1, y ], [ x + 1, y + 1 ], [ x + 1, y + 2 ] ],
+            [ [ x, y ], [ x, y ], [ x, y ], [ x, y ] ]
           ];
           this.position = 0;
         }
       };
 
+  // function defs
   // helper functions - draw boxes & text to correct scale
-  function strokeRec(x,y,w,h) {
-    ctx.strokeRect(x*pixel,y*pixel,w*pixel,h*pixel);
+  function strokeRec(x, y, w, h) {
+    ctx.strokeRect(x * pixel, y * pixel, w * pixel, h * pixel);
   }
-  function fillText(text,x,y) {
+  function fillText(text, x, y) {
     ctx.font="18px Georgia";
-    ctx.fillText(text,(x+0.25)*pixel,(y+0.75)*pixel);
+    ctx.fillText(text, (x + 0.25) * pixel, (y + 0.75) * pixel);
   }
-  function drawPixel(x,y) {
-    ctx.fillRect(x*pixel,y*pixel,1*pixel,1*pixel);
+  function drawPixel(x, y) {
+    ctx.fillRect(x * pixel, y * pixel, 1 * pixel, 1 * pixel);
   }
-  function drawBlock(coords,num) {
-    for (var i=0; i<num; i++) {
-      ctx.fillRect(coords[i][0]*pixel,coords[i][1]*pixel,1*pixel,1*pixel);
+  function drawBlock(coords, num) {
+    for (let i=0; i<num; i++) {
+      ctx.fillRect(coords[i][0] * pixel, coords[i][1] * pixel, 1 * pixel, 1 * pixel);
     }
   }
 
   // add a numbered grid to board.  for debugging
   function makeGrid() {
     for (var i=0; i<10; i++) {
-      strokeRec(i,0,1,20);
+      strokeRec(i, 0, 1, 20);
     }
     for (var i=0; i<20; i++) {
-      strokeRec(0,i,10,1);
+      strokeRec(0, i, 10, 1);
     }
     for (var i=0; i<20; i++) {
-      fillText(i,0,i);
+      fillText(i, 0, i);
     }
     for (var i=1; i<10; i++) {
-      fillText(i,i,0);
+      fillText(i, i, 0);
     }
   }
 
@@ -91,13 +115,13 @@
     if (falling) {
 
       // lower the block
-      for (var i=0; i<falling['coords'].length; i++) {
+      for (var i=0; i<falling.coords.length; i++) {
         falling['coords'][i][1]++;
       }
 
       // check if block is touching bottom now
       var touchingFloor = false;
-      for (var i=0; i<falling['coords'].length && touchingFloor===false; i++) {
+      for (var i=0; i<falling.coords.length && touchingFloor===false; i++) {
         if (falling['coords'][i][1] === 19) {
           touchingFloor = true;
         }
@@ -105,7 +129,7 @@
 
       // if at floor or , add block's pixels to landed array
       if (touchingFloor) {
-        for (var i=0; i<falling['coords'].length; i++) {
+        for (var i=0; i<falling.coords.length; i++) {
           var x = falling['coords'][i][0];
           var y = falling['coords'][i][1];
           landed[y][x] = 1;
@@ -116,7 +140,7 @@
 
         // check if touching another block
         var collision = false;
-        for (var i=0; i<falling['coords'].length; i++) {
+        for (var i=0; i<falling.coords.length; i++) {
           var x = falling['coords'][i][0];
           var y = falling['coords'][i][1] + 1;
           if (landed[y][x] === 1) {
@@ -124,7 +148,7 @@
           }
 
           if (collision) {
-            for (var i=0; i<falling['coords'].length; i++) {
+            for (var i=0; i<falling.coords.length; i++) {
               var x = falling['coords'][i][0];
               var y = falling['coords'][i][1];
               landed[y][x] = 1;
@@ -144,7 +168,7 @@
       // if not at left edge, move left
       var firstPixel = falling['coords'][0];
       if (firstPixel[0] > 0) {
-        for (var i=0; i<falling['coords'].length; i++) {
+        for (var i=0; i<falling.coords.length; i++) {
           falling['coords'][i][0]--;
         }
       }
@@ -152,10 +176,10 @@
 
     if (direction === 'right') {
       // if not at right edge, move right
-      var length = falling['coords'].length;
+      var length = falling.coords.length;
       var lastPixel = falling['coords'][length-1];
       if (lastPixel[0] < 9) {
-        for (var i=0; i<falling['coords'].length; i++) {
+        for (var i=0; i<falling.coords.length; i++) {
           falling['coords'][i][0]++;
         }
       }
@@ -168,6 +192,8 @@
     ctx.clearRect(0,0,10*pixel,20*pixel);
   }
 
+  // draw all pieces that have hit the bottom
+  // (this set grows as new pieces hit the bottom)
   function drawLanded() {
     for (var i=0; i<landed.length; i++) {
       for (var j=0; j<landed[i].length; j++) {
@@ -178,13 +204,17 @@
     }
   }
 
+  // spawns new block at top
+  // (todo: x-pos will be random & will account for block width
+  //        so not over either edge)
   function spawnBlock() {
     falling = new Block['i'](0,0);
   }
 
-  function rotate(x,y) {
+  // WIP block rotation function
+  function rotate() {
     falling.position++;
-    console.log(falling.position);
+    //console.log(falling.position);
     var position = falling.position;
     for (var i=0; i<falling.num; i++) {
       var newX = falling['positions'][position][i][0];
@@ -194,31 +224,38 @@
     }
   }
 
-  function processKeystroke(key){
+  // process all keystrokes
+  function processKeystroke(key) {
 
-    console.log(key);
+    if (!falling) {
+      return;
+    }
 
     // move block keyboard input
-    if (falling) {
-      switch (key) {
-        case 38:
-          rotate(falling['coords'][0][0],falling['coords'][0][1]);
-        case 40:
-          moveDown();
-          break;
-        case 39:
-          moveSide('right');
-          break;
-        case 37:
-          moveSide('left');
-          break;
-      }
+    switch (key) {
+
+      case 38:  // up arrow
+        //rotate(falling['coords'][0][0],falling['coords'][0][1]);
+        rotate();
+      case 40:  // down arrow
+        moveDown();
+        break;
+      case 39:  // right arrow
+        moveSide('right');
+        break;
+      case 37:  // left arrow
+        moveSide('left');
+        break;
     }
 
   }
 
-  var falling = new Block['i'](0,0);
+  function drawOnEvent(e) {
+    draw();
+    e.preventDefault();
+  }
 
+  // main draw loop (calls itself recursively at end)
   function draw() {
     if (frame % 100 === 0) {
       if (!falling) {
@@ -237,17 +274,27 @@
     requestAnimationFrame(draw);
   }
 
-  draw();
 
-  function drawOnEvent(e) {
-    draw();
-    e.preventDefault();
-  }
 
+  // event listeners
+  // for testing - "next" button below board
+  // (make sure moveDown() in draw() is uncommented)
   document.getElementById("next").addEventListener("click", drawOnEvent);
 
+  // event listener for all keystrokes
   document.onkeydown = function(e) {
     processKeystroke(e.keyCode);
   };
+
+
+
+
+  // placeholder starting block for testing
+  var falling = new Block['i'](0,0);
+
+  // call main draw loop
+  draw();
+
+
 
 })();
